be210f32ffb5788ce5edc4516f5e8589e8d71d5b
ChangJoo Park <pcjpcj2@gmail.com>
2016-11-21 17:27:51 +0900

    Add global styles to main.js

--- template/src/main.js
+++ template/src/main.js
@@ -5,6 +5,9 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
+// Styles
+import './assets/styles/styles.scss'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
