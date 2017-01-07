81c0b6615f87fc986617847f483c9525947fdcd7
ChangJoo Park <pcjpcj2@gmail.com>
2017-01-07 23:14:57 +0900

    Add Hello component

--- template/src/components/HomePage.vue
+++ template/src/components/HomePage.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <h1>Home</h1>
+    <hello></hello>
   </div>
 </template>
 
 <script>
+import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
 export default {
-  name: 'home-page'
-}
+  name: 'app',
+  components: {
+    Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
 <style lang="css" scoped>
